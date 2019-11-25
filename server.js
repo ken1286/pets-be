f71fd06813b36bbc761cc6274efcc851a0fc9c63
ken1286 <kindle1286@gmail.com>
2019-11-25 08:21:07 -0800

    basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+// const authRouter = require('./api/auth/auth-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/auth', authRouter);
+
+server.get('/', (req, res) => {
+  res.send('Server running...');
+});
+
+module.exports = server;
