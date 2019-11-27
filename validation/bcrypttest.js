f9a2491209bb0514a3c38fea81227e5311018f8d
ken1286 <kindle1286@gmail.com>
2019-11-27 02:01:29 -0800

    added put route

--- validation/bcrypttest.js
+++ validation/bcrypttest.js
@@ -1,4 +1,11 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const hash = bcrypt.hashSync('12345', 10); // 2 ^ n
-console.log(hash);
+// const hash = bcrypt.hashSync('12345', 10); // 2 ^ n
+// console.log(hash);
+
+const oldPet = { name: 'taco', species: 'dog', age: 5, stuff: 'stuff' };
+const changes = { name: 'tacos', species: 'dog', age: 4 };
+
+const newPet = { ...oldPet, ...changes };
+
+console.log(newPet);
